ac48eebbc8464058a08a1ec86d98a908fa2c6422
gabrielNZT <74830051+gabrielNZT@users.noreply.github.com>
2021-07-30 22:38:48 -0300

    Update Data.java

--- src/main/java/inventory/Data.java
+++ src/main/java/inventory/Data.java
@@ -1,17 +1,35 @@
 package inventory;
 import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.Vector;
 
 public class Data {
 
         private Vector<Product> products; // creation of vector products
         private Product product;
+        private Scanner input = new Scanner(System.in);
 
     public Data(){
 
         products = new Vector<Product>();
     }
     public void addProduct(Product product){
+        System.out.printf("Cadastre o produto %d:\n",product.getCod_register());
+
+        System.out.println("Informe o nome do produto:");
+        product.setName(input.next());
+
+        System.out.println("Informe a quantidade do produto:");
+        product.setAmount(input.nextInt());
+
+        System.out.println("Informe o preço do produto:");
+        product.setPrice(input.nextDouble());
+
+        System.out.println("Informe o código do produto:");
+        product.setCod(input.next());
+
+        product.setStatus(true);
+        System.out.printf("O produto %s foi cadastrado com sucesso:\nquantidade: %d\npreço: %f\ncódigo: %s\n",product.getName(),product.getAmount(),product.getPrice(),product.getCod());
          products.add(product);
 
     }
